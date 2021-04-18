aebe06eb88eb894f9bb57f9b8859269ffee9af4d
rod-da-god <stepnaerimov@gmail.com>
2021-04-18 16:15:39 +1000

    #1

---
+++ desktop/src/com/sung/project/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.sung.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sung.project.Chemb;
+import com.sung.project.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
