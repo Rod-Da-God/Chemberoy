88f838003a015d605d7572744217851cfb4aaf0a
rod-da-god <stepnaerimov@gmail.com>
2021-05-02 13:10:42 +1000

    4

--- core/src/com/sung/project/Main.java
+++ core/src/com/sung/project/Main.java
@@ -36,12 +36,14 @@ public class Main extends ApplicationAdapter implements Screen {
 	Texture _Attack;
 	Texture Ban;
 	Texture Bull;
+	Texture hills;
 
 
 
 	Sound bitSound;
 	Sound MhitSound;
 	Music backMusic;
+	Music cyber;
 
 
 	Vector3 vector3;//хрен знает зачем нужен...
@@ -60,6 +62,11 @@ public class Main extends ApplicationAdapter implements Screen {
 	Thread thread = new Thread();
 	Thread thread2 = new Thread();
 	Rectangle XY;
+	Rectangle Tgrass2;
+	Rectangle _anamy2;
+	Rectangle Forest;
+	Rectangle Hills;
+
 
 
 
@@ -96,6 +103,7 @@ public class Main extends ApplicationAdapter implements Screen {
 		_Attack = new Texture("attack.png");
 		Ban = new Texture("ban1.jpg");
 		Bull = new Texture("bull.png");
+		hills = new Texture("Hills.png");
 		scFont.getData().setScale(1.2f);
 		scFont.setColor(color);
 
@@ -106,6 +114,11 @@ public class Main extends ApplicationAdapter implements Screen {
 		backMusic.setVolume(0.3f);
 		backMusic.play();
 		MhitSound = Gdx.audio.newSound(Gdx.files.internal("hit.wav"));
+
+		cyber = Gdx.audio.newMusic(Gdx.files.internal("Cyber.mp3"));
+		cyber.setLooping(true);
+		cyber.setVolume(0.3f);
+
 	//MhitSound.loop(0.01f);
 
 
@@ -177,6 +190,33 @@ public class Main extends ApplicationAdapter implements Screen {
 		XY.x = player.x;
 
 
+		Tgrass2 = new Rectangle();
+		Tgrass2.x = 330;
+		Tgrass2.y = -50;
+		Tgrass2.height = 180;
+		Tgrass2.width = 500;
+
+		_anamy2 = new Rectangle();
+		_anamy2.x = 610;
+		_anamy2.y = 80;
+		_anamy2.width = 95;
+		_anamy2.height = 80;
+
+
+
+		Forest = new Rectangle();
+		Forest.x = 0;
+		Forest.y = 0;
+		Forest.width = 800;
+		Forest.height = 500;
+
+
+		Hills = new Rectangle();
+		Hills.x = 0;
+		Hills.y = 0;
+		Hills.width = 0;
+		Hills.height = 0;
+
 
 	}
 
@@ -188,13 +228,15 @@ public class Main extends ApplicationAdapter implements Screen {
 		Ocamera.update();
 		Sbatch.setProjectionMatrix(Ocamera.combined);
 		Sbatch.begin();
-		Sbatch.draw(forest, 0, 0, 800, 500);
+		Sbatch.draw(forest, Forest.x, Forest.y, Forest.width, Forest.height);
+		Sbatch.draw(hills,Hills.x,Hills.y,Hills.width,Hills.height);
 		Sbatch.draw(Tgrass, grass.x, grass.y, grass.width, grass.height);
 		Sbatch.draw(Tgrass, 180, -26, grass.width, 90);
-		Sbatch.draw(Tgrass, 330, -50, 500, 180);
+		Sbatch.draw(Tgrass, Tgrass2.x, Tgrass2.y, Tgrass2.width, Tgrass2.height);
 		//Sbatch.draw(Ship,ship.x,ship.y,ship.width,ship.height);
 		Sbatch.draw(Player, player.x, player.y, player.width, player.height);
 		Sbatch.draw(_Anamy, _anamy.x, _anamy.y, _anamy.width, _anamy.height);
+		Sbatch.draw(_Anamy, _anamy2.x , _anamy2.y, _anamy2.width, _anamy2.height);
 		Sbatch.draw(RightButton, right.x, right.y, right.width, right.height);
 		Sbatch.draw(LeftButton, left.x, left.y, left.width, left.height);
 		attack();
@@ -205,10 +247,16 @@ public class Main extends ApplicationAdapter implements Screen {
 		Sbatch.draw(Player, 15, 440, 40, 40);
 		Sbatch.draw(_Anamy,90,440,40,40);
 
-setLocation();
+
+
+		cleanMap();
 		walkRightLeft();
 		upTouch();
 		anamyWalk();
+		anamy2Walk();
+		invisible();
+		invisible2();
+
 
 		Sbatch.end();
 
@@ -248,6 +296,19 @@ setLocation();
 		}
 
 
+//vtoroy zombik
+		if (player.x  >=  _anamy2.x - 150 ){
+
+			_anamy2.x -= 120* Gdx.graphics.getDeltaTime();
+		}
+
+		if (player.x  <=  _anamy2.x +  150 ){
+			while (_anamy2.x <= player.x ){
+				_anamy2.x += 0.001;
+			}
+		}
+
+
 
 
 
@@ -266,15 +327,8 @@ setLocation();
 		 	player.y = 400;
 		 }
 
-		if (this._anamy.x <= 0) {
-			_anamy.x = 0;
 
-		}
 
-		if (_anamy.x >= 800 - 64){
-			_anamy.x = 800 - 64;
-
-		}
 
 
 		if (bullet.x >= 800 -64){
@@ -301,6 +355,15 @@ if (_anamy.overlaps(player) ){
 }
 
 
+		if (_anamy2.overlaps(player) ){
+			healthNow -=(0.1 - 0.01);
+			playerHealth = String.valueOf(healthNow);
+			player.x -= 2;
+			//MhitSound.play(0.02f);
+
+		}
+
+
 if (bullet.overlaps(_anamy)){
 	_anamy.setPosition(_anamy.x + 10,_anamy.y);
 	anamyHealthNow -=(0.1 - 0.01);
@@ -309,12 +372,41 @@ if (bullet.overlaps(_anamy)){
 	bullet.y = player.y+ 10;
 	if (anamyHealthNow <= 0){
 		_anamy.x = 0;
-		_anamy.y = 1480;
+		_anamy.y = 148000;
 	}
 }
 
 
 	}
+
+
+
+	void invisible2() {
+		if (this._anamy2.x <= 0) {
+			_anamy2.x = 0;
+
+		}
+
+		if (_anamy2.x >= 800 - 64) {
+			_anamy2.x = 800 - 64;
+
+		}
+	}
+
+
+	void invisible() {
+		if (this._anamy.x <= 0) {
+			_anamy.x = 0;
+
+		}
+
+		if (_anamy.x >= 800 - 64) {
+			_anamy.x = 800 - 64;
+
+		}
+	}
+
+
 private 	void walkRightLeft(){
 		if(Gdx.input.isTouched(0)) {
 			vector3.set(Gdx.input.getX(),Gdx.input.getY(), 0);
@@ -333,7 +425,7 @@ private 	void walkRightLeft(){
 
 	}
 
-private 	void anamyWalk() {
+ 	void anamyWalk() {
 		if (_anamy.y < grass.y - (grass.y * 2)) {
 			_anamy.y = grass.y - (grass.y * 2);
 
@@ -356,6 +448,35 @@ private 	void anamyWalk() {
 
 		} else {
 			_anamy.x = 650;
+
+		}
+	}
+
+
+
+	void anamy2Walk() {
+		if (_anamy2.y < grass.y - (grass.y * 2)) {
+			_anamy2.y = grass.y - (grass.y * 2);
+
+		} else _anamy2.y -= 50 * Gdx.graphics.getDeltaTime();
+
+		if (_anamy2.y < 30 + (-grass.y * 2) && _anamy2.x >= 180 && _anamy2.x <= 330) {
+			_anamy2.y = (-grass.y * 2);
+		} else _anamy2.y -= 50 * Gdx.graphics.getDeltaTime();
+
+		if (_anamy2.y < 30 + (-grass.y * 2) && _anamy2.x >= 330) {
+			_anamy2.y = 30 + (-grass.y * 2);
+		}
+		if (_anamy2.x >= 610 || _anamy2.x <= 610) {
+			if (_anamy2.x >= 610) {
+				_anamy2.x -= 10 * Gdx.graphics.getDeltaTime();
+			}
+			if (_anamy2.x <= 610) {
+				_anamy2.x += 30 * Gdx.graphics.getDeltaTime();
+			}
+
+		} else {
+			_anamy2.x = 610;
 
 		}
 	}
@@ -393,52 +514,57 @@ private 	void anamyWalk() {
 
 			}
 		}
-		void setLocation() {
-			//сделать доп условие
+
+
+
+		void cleanMap(){
 			if (anamyHealthNow <= 0) {
 
 
 				if (player.x > 800) {
+
+
+
 					grass.x = 0;
 					grass.y = 0;
 					grass.height = 0;
 					grass.width = 0;
 
-					player = new Rectangle();
+
 					player.x = 0;
 					player.y = 0;
 					player.height = 0;
 					player.width = 0;
 
-					right = new Rectangle();
+
 					right.x = 0;
 					right.y = 0;
 					right.height = 0;
 					right.width = 0;
 
 
-					left = new Rectangle();
+
 					left.x = 0;
 					left.y = 0;
 					left.height = 0;
 					left.width = 0;
 
 
-					up = new Rectangle();
+
 					up.x = 0;
 					up.y = 0;
 					up.height = 0;
 					up.width = 0;
 
 
-					ship = new Rectangle();
+
 					ship.x = 0;
 					ship.y = 0;
 					ship.width = 0;
 					ship.height = 0;
 
 
-					_anamy = new Rectangle();
+
 					_anamy.x = 0;
 					_anamy.y = 0;
 					_anamy.width = 0;
@@ -446,36 +572,62 @@ private 	void anamyWalk() {
 					playerHealth = String.valueOf(healthNow);
 
 
-					_attack = new Rectangle();
 					_attack.x = 0;
 					_attack.y = 0;
 					_attack.height = 0;
 					_attack.width = 0;
 
 
-					ban = new Rectangle();
+
 					ban.width = 0;
 					ban.height = 0;
 
-					bullet = new Rectangle();
+
 					bullet.height = 0;
 					bullet.width = 0;
 
-					XY = new Rectangle();
+
 					XY.x = player.x;
-					_Anamy.dispose();
-					Ship.dispose();
-					scFont.dispose();
-					forest.dispose();
-					Tgrass.dispose();
-					UpButton.dispose();
-					LeftButton.dispose();
-					RightButton.dispose();
-					Player.dispose();
-					backMusic.dispose();
+
+
+					Tgrass2.x = 0;
+					Tgrass2.y = 0;
+					Tgrass2.height = 0;
+					Tgrass2.width = 0;
+
+
+					_anamy2.x = 0;
+					_anamy2.y = 0;
+					_anamy2.width = 0;
+					_anamy2.height = 0;
+
+					Forest.x = - 800;
+					Forest.y = - 800;
+					Forest.width = 0;
+					Forest.height = 0;
+					backMusic.stop();
+
+
+newLocation();
 					// вот тут заново все рисовать и параметры настроить!!
+
+
+
 				}
 			}
+		}
+
+
+		void newLocation(){
+			Hills.height = 500;
+			Hills.width = 800;
+			Hills.x = 0;
+			Hills.y = 0;
+
+			int health2 = 1000;
+			playerHealth = String.valueOf(health2);
+			cyber.play();
+
 		}
 
 
